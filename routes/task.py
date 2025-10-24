99546d97d47f3e14e1284ce9bf2e0f195f5f6d2c
KSSIRI05 <kss.mohammed05@gmail.com>
2025-10-24 23:32:08 +0100

    ajout des exception

--- routes/task.py
+++ routes/task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from APIproject.models.task import Task
 from APIproject.schema.task import CreateTask,UpdateTask 
 
@@ -7,35 +7,87 @@ router= APIRouter()
 @router.get("/api/v1/task/{id}")
 async def get_task(id):
     task = await Task.get(id)
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail=f"tache avec l'{id} non trouvee"
+        )
     return  task
 
 @router.get("/api/v1/tasks")
 async def get_alltasks():
     tasks = await Task.find_all().to_list()
+    if not tasks:
+        raise HTTPException(
+            status_code=404,
+            detail="aucune tache est trouvee"
+        )
     return tasks
+        
+   
 
 @router.post("/api/v1/tasks")
 async def cretae_task(data : CreateTask):
-    task = Task(**data.dict())
-    await task.create()
-    return task
+    try:
+        task = Task(**data.dict())
+        await task.create()
+        return task
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Erreur lors de la création de la tâche : {str(e)}"
+        )
+    
 
 @router.put("/api/v1/task/{id}")
 async def update_task(id: str ,data : UpdateTask):
     task = await Task.get(id)
-    for field, value in data.dict().items():
-        setattr(task, field, value)
-    return task
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail=f"tache avec l'{id} non trouvee"
+        )
+    try:
+        for field, value in data.dict().items():
+            setattr(task, field, value)
+        return task
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Erreur lors de la moification de la tâche : {str(e)}"
+        )
 
 @router.patch("/api/v1/task/{id}")
 async def patch_task(id: str ,data : UpdateTask):
     task = await Task.get(id)
-    for field, value in data.dict().items():
-        setattr(task, field, value)
-    return task
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail=f"tache avec l'{id} non trouvee"
+        )
+    try :  
+        for field, value in data.dict().items():
+            setattr(task, field, value)
+        return task
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Erreur lors de la modification de la tâche : {str(e)}"
+        )
 
 @router.delete("/api/v1/task/{id}")
 async def delete_task(id):
     task = await Task.get(id)
-    await task.delete
-    return  "suprrime"
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail=f"tache avec l'{id} non trouvee"
+        )
+    try :
+        await task.delete
+        return  "suprrime"
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erreur lors de la supression de la tâche : {str(e)}"
+        )
