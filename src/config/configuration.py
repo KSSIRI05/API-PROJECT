8dc680ffbcaa415a1516fe930c7226f4736d7e8a
KSSIRI05 <kss.mohammed05@gmail.com>
2026-04-23 21:42:43 +0100

    modification de config

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,19 +1,18 @@
-# config/database.py
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from src.models.task import Task  # modèle à enregistrer dans Beanie
+from src.models.task import Task
 import os
-
 from dotenv import load_dotenv
+
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
+_client = None  # keep reference to avoid GC
+
 async def init_db():
-    client = AsyncIOMotorClient(MONGO_URI, serverSelectionTimeoutMS=60000)  # 60 secondes au lieu de 30
-    # on sélectionne la base de données
-    database = client["API-DATA"]
-    
-    # initialisation de Beanie avec la base et les modèles
+    global _client
+    _client = AsyncIOMotorClient(MONGO_URI, serverSelectionTimeoutMS=60000)
+    database = _client["API-DATA"]
     await init_beanie(database=database, document_models=[Task])
 
 async def check_db_connection():
